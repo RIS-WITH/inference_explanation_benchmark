dc44542d51116b5d197275ce6dda204c2d6b7c8a
BastienDssrd <bastien.dussard@gmail.com>
2024-12-24 14:43:33 +0100

    small fixes and PromptGenerator

--- generate_questions.py
+++ generate_questions.py
@@ -1,9 +1,10 @@
 from data.questions import question_1, question_2, question_3
 from data.examples import example_0, example_1
+from data.prompts import prompt_test
 
 from src.DataHandler import QuestionHandler
 from src.ExampleHandler import ExampleHandler
-from src.QuestionGenerator import QuestionGenerator
+from src.QuestionGenerator import QuestionGenerator, PromptGenerator
 
 if __name__ == '__main__':
     print(" -- hellollama : generating questions -- ")
@@ -12,8 +13,10 @@ if __name__ == '__main__':
 
     # set the list of questions
     question_list = [question_1, question_2, question_3]
+    
+    prompt_generator = PromptGenerator(prompt_test)
 
-    generator = QuestionGenerator()
+    generator = QuestionGenerator(prompt_generator)
     
     saving_path = "/home/bdussard/"
     directory_name = "dataset_q3"
@@ -25,11 +28,11 @@ if __name__ == '__main__':
 
     # ============== ADDING QUESTIONS TO GENERATOR ===============
     for question in question_list:
-        generator.add_question(question)
+        generator.add_question(question, with_CoT=True)
 
     # # =============== GENERATE QUESTION VARIATIONS ===========
     generator.generate_question_variations(nb_var=10)
     
      # ============== GENERATE QUESTION FILES AND SAVE TO FILE ===============
     for question in generator.questions_:
-        data_saver.save_question(question, generator)
+        data_saver.create_and_save_question_variation(question, generator)
