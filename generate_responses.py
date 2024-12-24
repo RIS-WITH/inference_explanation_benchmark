d8a31d66b38c80300c8340bdf5a7516696cfced0
BastienDssrd <bastien.dussard@gmail.com>
2024-12-24 14:43:47 +0100

    small modifs

--- generate_responses.py
+++ generate_responses.py
@@ -7,16 +7,18 @@ if __name__ == '__main__':
     #models = ["llama3.2", "llama3.1", "gemma2:2b", "gemma2", "mistral", "phi3", "phi3:medium"]
     # set the list of examples
 
-    model = "llama3.2"
+    model = "gemma2:27b"
+
     ollama = OllamaHandler(model)
  
     question_path = "/home/bdussard/"
     questions_folder = "dataset_q3"
     
-    saving_path_answers = "/home/bdussard/answer_results_test/"
-    directory_name = model
+    answer_path = "/home/bdussard/"
+    answer_folder = "answer_folder_noCoT"
+    answer_model = model
     
-    answer_saver = AnswerHandler(saving_path_answers, model)
+    answer_saver = AnswerHandler(answer_path, answer_folder, model)
     
      #  ================ LOAD QUESTIONS FROM FILE =============
     questions_baseline = answer_saver.load_questions(question_path + questions_folder, "baseline")
@@ -27,12 +29,12 @@ if __name__ == '__main__':
     
     for question_key in questions_dict:
         print(" ===  evaluating questions ", question_key)
-        answer_saver.create_answer_folder(question_key)
+        answer_saver.create_variation_folder(question_key)
         for question in questions_dict[question_key]:
             id_question = question['id']
             filename_answer = answer_saver.create_answer_file(id_question, question_key, question)
             for question_var in question['questions']:
-                formatted_question = ollama.build_question(question, question_var['question'])
+                formatted_question = ollama.build_question(question, question_var['question'], with_CoT=True)
                 print(" -- question is : ", formatted_question[-2]['content'])
                 answer = ollama.call(formatted_question)
                 print(" -- answer is :", answer)
