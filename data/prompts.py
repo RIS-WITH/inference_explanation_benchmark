f6d2f6ccf340d4ced6ef1680b6d4808defd3279c
BastienDssrd <bastien.dussard@gmail.com>
2025-01-03 14:55:47 +0100

    change on initial prompt

--- data/prompts.py
+++ data/prompts.py
@@ -1,37 +1,53 @@
 
 
-prompt_0 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of explanations for an \
+prompt_0 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of justifications for an \
           inference into a compact and simple sentence in natural langage."
           
-prompt_1 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of explanations for an \
+prompt_1 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of justifications for an \
           inference into a compact and simple sentence in natural langage. \
           In description logics, x|isA|Y represent an inheritance relation, while x|property|z represents a relation between two individuals."
           
-prompt_2 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of explanations for an \
+prompt_2 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of justifications for an \
           inference into a compact and simple sentence in natural langage. \
           In description logics, x|isA|Y represent an inheritance relation, while x|property|z represents a relation between two individuals. \
           The explanation should not refer to the individual names, but rather the semantic concepts they represent."
           
-prompt_3 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of explanations for an\
+prompt_3 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of justifications for an\
             inference into a compact and simple sentence in natural langage. The translation must contain the least number sentences as possible, \
             and you mustn't describe the reasoning process. In description logics, x|isA|Y represent an inheritance relation, while x|property|z \
             represents a relation between two individuals. The explanation should not refer to the individual names, but rather the semantic concepts \
-            they represent. For instance, x|isA|Cup, x|isEmpty|boolean#true would translate to the cup is empty. The inferences you will have to translate are about why an agent can do a particular action. The explanations are all important and must be all used in order to generate the translation."
+            they represent. For instance, x|isA|Cup, x|isEmpty|boolean#true would translate to the cup is empty. The inferences you will have to translate are about why an agent can do a particular action. The justifications are all important and must be all used in order to generate the translation."
 
-prompt_init = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of explanations for an\
+prompt_init = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of justifications for an\
                inference into a compact and simple sentence in natural langage."
                
-prompt_test = "You are an AI model specialized in transforming formal logical inferences and their explanations into clear and concise natural language sentences.\
+prompt_test = "You are an AI model specialized in transforming formal logical inferences and their justifications into clear and concise natural language sentences.\
     In this task: \n\
-    \t You will interpret inferences and explanations derived from the resolution of SWRL rules. \n\
+    \t You will interpret inferences and justifications derived from the resolution of SWRL rules. \n\
     \t SWRL rules are used in ontologies to infer new facts based on known facts and logical conditions. \n\
-    \t Each inference is accompanied by a set of explanations in the form of triples. These triples specify relationships between entities or concepts, such as xhz|isA|Human (which means xhz is an instance of the class Human). \n\
+    \t Each inference is accompanied by a set of justifications in the form of triples. These triples specify relationships between entities or concepts, such as xhz|isA|Human (which means xhz is an instance of the class Human). \n\
     Your goal: \n\
-    \tConvert the SWRL inference and its supporting explanations into a natural language sentence. \n\
+    \tConvert the SWRL inference and its supporting justifications into a natural language sentence. \n\
     \tEnsure the explanation is clear, concise, and accessible to someone unfamiliar with SWRL or Description Logics. \n\
-    Here is an example: Input: Inference: xhz|hasBrother|okm Explanations: xhz|isA|Human (xhz is a Human), xhz|hasSibling|okm (xhz has a sibling okm), okm|isA|Man (xhz is a Man)\
+    Here is an example: Input: Inference: xhz|hasBrother|okm justifications: xhz|isA|Human (xhz is a Human), xhz|hasSibling|okm (xhz has a sibling okm), okm|isA|Man (xhz is a Man)\
     Output in Natural Language : The human xhz has a brother named okm because xhz is a human, who has okm as a sibling, who is a man."
-    #Output in Natural Language : xhz has a brother named okm because xhz is a human, okm is a man, and okm is a sibling of xhz."
+
+prompt_rdf = "You are an AI model specialized in transforming formal logical inferences and their justifications into clear and concise natural language sentences.\
+    In this task: \n\
+    \t You will interpret inferences and justifications derived from the resolution of SWRL rules. \n\
+    \t SWRL rules are used in ontologies to infer new facts based on known facts and logical conditions. \n\
+    \t Each inference is accompanied by a set of justifications in the form of triples. These triples specify semantic relationships between entities or concepts such as\
+    indiv_1|Type|ConceptX (which means the individual indiv_1 is an instance of the class ConceptX),\
+    ConceptX|SubClassOf|ConceptY (which means that ConceptX inherits from ConceptY),\
+    indiv_0|hasXProperty|indiv_1 (indiv_0 is related to indiv_1 through the semantics of hasXProperty),\
+    or indiv_2|hasYProperty|boolean#false (indiv_2 has the datatype value boolean#false to the property hasYProperty).\
+    Your goal: \n\
+    \tConvert the SWRL inference and its supporting justifications into a natural language sentence. \n\
+    \tEnsure the explanation is clear, concise, and accessible to someone unfamiliar with SWRL or Description Logics. \n\
+    \t The generated explanation must be one or few natural language sentences, and not a list or bullet points. \n\
+    \t The generated explanation must not contain the names of the individuals, but only the concepts and properties (indiv_1|Type|Human translates to The human) \n\
+    Here is an example: Input: Inference: xhz|hasBrother|okm / justifications: xhz|Type|Human, xhz|hasSibling|okm, okm|Type|Man\
+    Output in Natural Language : The human has a brother because he has a sibling who is a man."
 
 prompt_0_clean = prompt_0.replace('\t', '')
 prompt_1_clean = prompt_1.replace('\t', '')
