f65039b6bf2001ee14381a32183852859bd942de
BastienDssrd <bastien.dussard@gmail.com>
2025-01-05 14:17:24 +0100

    updating script to generate question with prompt without ontology explanations

--- generate_questions.py
+++ generate_questions.py
@@ -9,8 +9,8 @@ from data.canLift import question_lift_easy, question_lift_medium, question_lift
 from data.canPush import question_push_easy, question_push_medium, question_push_hard
 from data.canPerceive import question_perceive_easy, question_perceive_medium, question_perceive_hard
 
-from data.examples import example_0, example_1, example_2
-from data.prompts import prompt_test, prompt_rdf
+from data.examples import example_0, example_1, example_2, example_3
+from data.prompts import prompt_test, prompt_rdf, prompt_with_ontology_explanations, prompt_without_ontology_explanations
 
 from src.DataHandler import QuestionHandler
 from src.ExampleHandler import ExampleHandler
@@ -18,7 +18,7 @@ from src.QuestionGenerator import QuestionGenerator, PromptGenerator
 
 if __name__ == '__main__':
     print(" -- hellollama : generating questions -- ")
-    examples_list = [example_1, example_2]
+    examples_list = [example_1, example_2, example_3]
     example_handler = ExampleHandler(examples_list)
 
     # set the list of questions
@@ -28,12 +28,12 @@ if __name__ == '__main__':
                      question_push_easy, question_push_medium, question_push_hard,
                      question_perceive_easy, question_perceive_medium, question_perceive_hard]
     
-    prompt_generator = PromptGenerator(prompt_rdf)
+    prompt_generator = PromptGenerator(prompt_without_ontology_explanations)
 
     generator = QuestionGenerator(prompt_generator)
     
     saving_path = "/home/bdussard/"
-    directory_name = "dataset_questions_glpp"
+    directory_name = "dataset_questions_wihtout_explanations"
     data_saver = QuestionHandler(saving_path, directory_name)
  
     # ============== ADDING EXAMPLES TO GENERATOR ================
