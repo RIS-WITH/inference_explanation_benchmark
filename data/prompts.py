714a1e772d554a1173af2016c3318d774487a5b7
BastienDssrd <bastien.dussard@gmail.com>
2025-03-13 11:14:34 +0100

    cleaning old prompts

--- data/prompts.py
+++ data/prompts.py
@@ -1,58 +1,3 @@
-
-
-prompt_0 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of justifications for an \
-          inference into a compact and simple sentence in natural langage."
-          
-prompt_1 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of justifications for an \
-          inference into a compact and simple sentence in natural langage. \
-          In description logics, x|isA|Y represent an inheritance relation, while x|property|z represents a relation between two individuals."
-          
-prompt_2 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of justifications for an \
-          inference into a compact and simple sentence in natural langage. \
-          In description logics, x|isA|Y represent an inheritance relation, while x|property|z represents a relation between two individuals. \
-          The explanation should not refer to the individual names, but rather the semantic concepts they represent."
-          
-prompt_3 = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of justifications for an\
-            inference into a compact and simple sentence in natural langage. The translation must contain the least number sentences as possible, \
-            and you mustn't describe the reasoning process. In description logics, x|isA|Y represent an inheritance relation, while x|property|z \
-            represents a relation between two individuals. The explanation should not refer to the individual names, but rather the semantic concepts \
-            they represent. For instance, x|isA|Cup, x|isEmpty|boolean#true would translate to the cup is empty. The inferences you will have to translate are about why an agent can do a particular action. The justifications are all important and must be all used in order to generate the translation."
-
-prompt_init = "As an expert translator from description logics in ontologies to natural language, your job is to transform the chain of justifications for an\
-               inference into a compact and simple sentence in natural langage."
-               
-prompt_test = "You are an AI model specialized in transforming formal logical inferences and their justifications into clear and concise natural language sentences.\
-    In this task: \n\
-    \t You will interpret inferences and justifications derived from the resolution of SWRL rules. \n\
-    \t SWRL rules are used in ontologies to infer new facts based on known facts and logical conditions. \n\
-    \t Each inference is accompanied by a set of justifications in the form of triples. These triples specify relationships between entities or concepts, such as xhz|isA|Human (which means xhz is an instance of the class Human). \n\
-    Your goal: \n\
-    \tConvert the SWRL inference and its supporting justifications into a natural language sentence. \n\
-    \tEnsure the explanation is clear, concise, and accessible to someone unfamiliar with SWRL or Description Logics. \n\
-    Here is an example: Input: Inference: xhz|hasBrother|okm justifications: xhz|isA|Human (xhz is a Human), xhz|hasSibling|okm (xhz has a sibling okm), okm|isA|Man (xhz is a Man)\
-    Output in Natural Language : The human xhz has a brother named okm because xhz is a human, who has okm as a sibling, who is a man."
-
-prompt_rdf = "You are an AI model specialized in transforming formal logical inferences and their justifications into clear and concise natural language sentences.\
-    In this task: \n\
-    \t You will interpret inferences and justifications derived from the resolution of SWRL rules. \n\
-    \t SWRL rules are used in ontologies to infer new facts based on known facts and logical conditions. \n\
-    \t Each inference is accompanied by a set of justifications in the form of triples. \
-    These triples specify semantic relationships between entities or concepts such as\
-    indiv_1|Type|ConceptX (which means the individual indiv_1 is an instance of the class ConceptX),\
-    ConceptX|SubClassOf|ConceptY (which means that ConceptX inherits from ConceptY),\
-    ConceptX|EquivalentTo|(hasXProperty some ConceptY) (which means that all individuals matching the hasXProperty some ConceptY field will inherit from ConceptX),\
-    indiv_0|hasXProperty|indiv_1 (indiv_0 is related to indiv_1 through the semantics of hasXProperty),\
-    or indiv_2|hasYProperty|boolean#false (indiv_2 has the datatype value boolean#false to the property hasYProperty).\
-    (hasXProperty o hasYProperty)|SubPropertyOf|hasZProperty corresponds to a property chain axiom.\
-    Your goal: \n\
-    \tConvert the SWRL inference and its supporting justifications into a natural language sentence. \n\
-    \tEnsure the explanation is clear, concise, and accessible to someone unfamiliar with SWRL or Description Logics. \n\
-    \tThe generated explanation must be one or few natural language sentences, and not a list or bullet points, it\
-    must not contain the names of the individuals, but only the concepts and properties (indiv_1|Type|Human translates to The human),\
-    and should contain all of the justifications for that inference to have been made. \n\
-    Here is an example: Input: Inference: xhz|hasBrother|okm / Justifications: xhz|Type|Human, xhz|hasSibling|okm, okm|Type|Man\
-    Output : The human has a brother because he has a sibling who is a man."
-
 prompt_without_ontology_explanations = "You are an AI model specialized in explaining formal logical inferences and their justifications in natural language. \n\
             Task Overview:\
             Convert inferences and justifications from SWRL rules into concise, clear, and natural language explanations. \
@@ -63,7 +8,7 @@ prompt_without_ontology_explanations = "You are an AI model specialized in expla
             \tRule: The SWRL rule used to make such an inference.\n\
             Output Requirements:\n\
             \tClarity: Write a single, fluent sentence or short paragraph without any break-down or bullet points.\n\
-            \tCompleteness: Include all relevant justifications into the explanation.\n\
+            \tCompleteness: Include all justifications into the explanation.\n\
             \tConcept-Driven: Don't refer to individual names (e.g., indiv_1|Type|Human becomes 'The human').\n\
             \tEdge Case:  For inferences involving chains, equivalent concepts, subclasses or subproperties, explicitly reflect these relationships in the explanation.\n\
             Examples:\n\
@@ -85,21 +30,12 @@ prompt_with_ontology_explanations = "You are an AI model specialized in explaini
             \tRule: The SWRL rule used to make such an inference.\n\
             Output Requirements:\n\
             \tClarity: Write a single, fluent sentence or short paragraph without any break-down or bullet points.\n\
-            \tCompleteness: Include all relevant justifications into the explanation.\n\
+            \tCompleteness: Include all justifications into the explanation.\n\
             \tConcept-Driven: Don't refer to individual names (e.g., indiv_1|Type|Human becomes 'The human').\n\
             \tEdge Case:  For inferences involving chains, equivalent concepts, subclasses or subproperties, explicitly reflect these relationships in the explanation.\n\
             Examples:\n\
             Input: -Inference: xhz|hasBrother|okm\n -Justifications: xhz|Type|Human, xhz|hasSibling|okm, okm|Type|Man\n -Rule: Human(?a), hasSibling(?a,?b), Man(?b) -> hasBrother(?a,?b).\n\
             Output: 'The human has a brother because he has a sibling who is a man'."
 
-prompt_0_clean = prompt_0.replace('\t', '')
-prompt_1_clean = prompt_1.replace('\t', '')
-prompt_2_clean = prompt_2.replace('\t', '')
-prompt_3_clean = prompt_3.strip('\t')
-
-print(prompt_3_clean)
-          
-
-          
 
           
