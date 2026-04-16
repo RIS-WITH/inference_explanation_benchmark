ed2f77ab11ef768dca174399f99db657f7dd15a8
BastienDssrd <bastien.dussard@gmail.com>
2026-04-16 12:48:34 +0200

    adding the models in config file

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 DATASET_DIR = BASE_DIR / "dataset_2026"
-MODELS = ["llama3.2:3b"] #, "llama3.1:8b", "gemma2:2b", "gemma2:9b", "mistral-nemo:12b", "mistral-small:22b"]
+MODELS = ["llama3.2:3b", "gemma3:4b"] #, "llama3.1:8b", "gemma2:2b", "gemma2:9b", "mistral-nemo:12b", "mistral-small:22b"]
